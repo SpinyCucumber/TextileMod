79ecb67825d4c2f7464e4c60d62a4f4f53124d01
Elijah Hilty <Elijah Hilty@DESKTOP-86TBM65.attlocal.net>
2020-08-11 17:55:05 -0500

    Added basin filling functionality

--- src/main/java/spinyq/spiny_textiles/blocks/BasinBlock.java
+++ src/main/java/spinyq/spiny_textiles/blocks/BasinBlock.java
@@ -2,18 +2,26 @@ package spinyq.spiny_textiles.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.pathfinding.PathType;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.shapes.IBooleanFunction;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoaderRegistry;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import spinyq.spiny_textiles.TextileMod;
 import spinyq.spiny_textiles.tiles.BasinTile;
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
@@ -26,11 +34,43 @@ public class BasinBlock extends Block {
 					makeCuboidShape(4.0D, 0.0D, 0.0D, 12.0D, 3.0D, 16.0D),
 					makeCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 3.0D, 14.0D), INSIDE),
 			IBooleanFunction.ONLY_FIRST);
-	
-	@SubscribeEvent
-	public static void registerModels(ModelRegistryEvent event) {
-		// TODO Setup model
-		ModelLoaderRegistry.registerLoader(null, null);
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player,
+			Hand handIn, BlockRayTraceResult hit) {
+		// Check that the player is indeed interacting with the tile entity
+		TextileMod.LOGGER.info("BasinBlock Activated...");
+		TileEntity tile = world.getTileEntity(pos);
+		
+		if (tile instanceof BasinTile) {
+			
+			BasinTile basin = (BasinTile) tile;
+			ItemStack itemstack = player.getHeldItem(handIn);
+			// Only care about clicking with items
+			if (itemstack.isEmpty()) {
+				return ActionResultType.PASS;
+			} else {
+				// Adapted from cauldron code
+				Item item = itemstack.getItem();
+				if (item == Items.WATER_BUCKET && basin.isEmpty()) {
+					TextileMod.LOGGER.info("BasinBlock Water Bucket...");
+					
+					if (!world.isRemote) {
+						if (!player.abilities.isCreativeMode) {
+							player.setHeldItem(handIn, new ItemStack(Items.BUCKET));
+						}
+						basin.fill();
+						world.playSound((PlayerEntity) null, pos, SoundEvents.ITEM_BUCKET_EMPTY,
+								SoundCategory.BLOCKS, 1.0F, 1.0F);
+					}
+					
+					return ActionResultType.SUCCESS;
+				}
+			}
+		}
+		
+		return super.onBlockActivated(state, world, pos, player, handIn, hit);
 	}
 
 	@Override
--- src/main/java/spinyq/spiny_textiles/tiles/BasinTile.java
+++ src/main/java/spinyq/spiny_textiles/tiles/BasinTile.java
@@ -1,24 +1,23 @@
 package spinyq.spiny_textiles.tiles;
 
-import java.util.Random;
-
+import net.minecraft.block.BlockState;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SUpdateTileEntityPacket;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.util.Constants;
 import spinyq.spiny_textiles.ModTiles;
 import spinyq.spiny_textiles.util.Color3f;
 
 public class BasinTile extends TileEntity {
-
-	private static final Random RANDOM = new Random();
-	
+		
 	public static final int MAX_WATER_LEVEL = 8, MAX_DYE_LEVEL = 8;
 	
 	public BasinTile() {
 		super(ModTiles.BASIN_TILE.get());
-		// TODO Remove this, just for testing
-		waterLevel = MAX_WATER_LEVEL - 1;
-		color = Color3f.random(RANDOM);
-		dyeLevel = RANDOM.nextInt(MAX_DYE_LEVEL);
+		waterLevel = 0;
+		color = new Color3f();
+		dyeLevel = 0;
 	}
 
 	private static final String KEY_WATER_LEVEL = "WaterLevel", KEY_DYE_LEVEL = "DyeLevel", KEY_COLOR = "Color";
@@ -34,12 +33,35 @@ public class BasinTile extends TileEntity {
 	}
 	
 	@Override
-	public CompoundNBT write(CompoundNBT compound) {
-		CompoundNBT tag = new CompoundNBT();
+	public CompoundNBT write(CompoundNBT tag) {
 		tag.putInt(KEY_WATER_LEVEL, waterLevel);
 		tag.putInt(KEY_DYE_LEVEL, dyeLevel);
 		tag.putInt(KEY_COLOR, color.toInt());
 		return tag;
+	}
+
+	@Override
+	public SUpdateTileEntityPacket getUpdatePacket() {
+		CompoundNBT nbtTag = new CompoundNBT();
+	    //Write your data into the nbtTag
+		write(nbtTag);
+	    return new SUpdateTileEntityPacket(getPos(), -1, nbtTag);
+	}
+
+	@Override
+	public void onDataPacket(NetworkManager net, SUpdateTileEntityPacket pkt) {
+		CompoundNBT tag = pkt.getNbtCompound();
+	    //Handle your Data
+		read(tag);
+	}
+	
+	/**
+	 * Syncs data with clients and marks this tile to be saved.
+	 */
+	private void update() {
+		BlockState state = world.getBlockState(pos);
+		world.notifyBlockUpdate(pos, state, state, Constants.BlockFlags.BLOCK_UPDATE);
+		markDirty();
 	}
 
 	public int getWaterLevel() {
@@ -48,6 +70,39 @@ public class BasinTile extends TileEntity {
 
 	public int getDyeLevel() {
 		return dyeLevel;
+	}
+
+	/**
+	 * Sets the water level. Sends data to clients.
+	 * @param waterLevel
+	 */
+	public void setWaterLevel(int waterLevel) {
+		this.waterLevel = waterLevel;
+		update();
+	}
+
+	/**
+	 * Sets the dye level. Sends data to clients.
+	 * @param dyeLevel
+	 */
+	public void setDyeLevel(int dyeLevel) {
+		this.dyeLevel = dyeLevel;
+		update();
+	}
+	
+	/**
+	 * Sets the water level to the max.
+	 */
+	public void fill() {
+		setWaterLevel(MAX_WATER_LEVEL - 1);
+	}
+	
+	/**
+	 * Whether the basin contains any water.
+	 * @return
+	 */
+	public boolean isEmpty() {
+		return waterLevel == 0;
 	}
 
 	public float getDyeConcentration() {
