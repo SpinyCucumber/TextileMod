f935b7ba5e53e45965bcb4a9d819bc5843356dbc
Elijah Hilty <Elijah Hilty@DESKTOP-86TBM65.attlocal.net>
2020-08-10 19:53:10 -0500

    Added new fields to BasinTile

--- src/main/java/spinyq/spiny_textiles/tiles/BasinTile.java
+++ src/main/java/spinyq/spiny_textiles/tiles/BasinTile.java
@@ -11,13 +11,14 @@ public class BasinTile extends TileEntity {
 		super(ModTiles.BASIN_TILE.get());
 	}
 
-	private static final String KEY_DYE_LEVEL = "dyeLevel", KEY_COLOR = "color";
+	private static final String KEY_WATER_LEVEL = "WaterLevel", KEY_DYE_LEVEL = "DyeLevel", KEY_COLOR = "Color";
 	
-	private int dyeCapacity, dyeLevel;
+	private int waterLevel, dyeLevel;
 	private Color color;
 	
 	@Override
 	public void read(CompoundNBT compound) {
+		waterLevel = compound.getInt(KEY_WATER_LEVEL);
 		dyeLevel = compound.getInt(KEY_DYE_LEVEL);
 		color = Color.fromInt(compound.getInt(KEY_COLOR));
 	}
@@ -25,13 +26,14 @@ public class BasinTile extends TileEntity {
 	@Override
 	public CompoundNBT write(CompoundNBT compound) {
 		CompoundNBT tag = new CompoundNBT();
+		tag.putInt(KEY_WATER_LEVEL, waterLevel);
 		tag.putInt(KEY_DYE_LEVEL, dyeLevel);
 		tag.putInt(KEY_COLOR, color.toInt());
 		return tag;
 	}
 
-	public double getConcentration() {
-		return (double) dyeLevel / (double) dyeCapacity;
+	public double getDyeConcentration() {
+		return (double) dyeLevel / (double) waterLevel;
 	}
 	
 	public Color getColor() {
