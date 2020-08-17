cc5c0d4bfca6853011cb72acdb7d81507b6b6964
Elijah Hilty <spinycucumber@gmail.com>
2020-08-16 23:18:01 -0500

    More work on revamping model system

--- src/main/java/spinyq/spinytextiles/client/render/BasinRenderer.java
+++ src/main/java/spinyq/spinytextiles/client/render/BasinRenderer.java
@@ -5,7 +5,9 @@ import com.mojang.blaze3d.vertex.IVertexBuilder;
 
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.model.Material;
 import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.client.renderer.texture.AtlasTexture;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.util.ResourceLocation;
@@ -21,12 +23,14 @@ public class BasinRenderer extends TileEntityRenderer<BasinTile> {
 	private static final Color3f WATER_COLOR = Color3f.fromIntString("0x3F76E4");
 	private static final ResourceLocation WATER_TEXTURE = new ResourceLocation("textures/block/water_still.png");
 	private static final RenderType WATER_RENDER_TYPE = RenderType.getEntityTranslucent(WATER_TEXTURE);
+	@SuppressWarnings("deprecation")
+	private static final Material WATER_MATERIAL = new Material(AtlasTexture.LOCATION_BLOCKS_TEXTURE, new ResourceLocation("block/dirt"));
 	private static final int STAGES = 200;
 
 	private final ModelRenderer[] fluidModels = new ModelRenderer[STAGES];
 
 	private ModelRenderer getFluidModel(int stage) {
-		ModelRenderer model = new ModelRenderer(16, 512, 0, 0);
+		ModelRenderer model = new ModelRenderer(16, 16, 0, 0);
 		model.addBox(2.0f + 0.01f, 3.0f + .01f, 2.0f + 0.01f, 12.0f - 0.01f,
 				((float) stage / (float) STAGES) * 12.0f - 0.01f, 12.0f - 0.01f);
 		return model;
@@ -57,7 +61,7 @@ public class BasinRenderer extends TileEntityRenderer<BasinTile> {
 			Color3f rgb = WATER_COLOR.lerp(basin.getColor(), conc);
 			Color4f color = new Color4f(rgb, 1.0f);
 			// Allocate buffer
-			IVertexBuilder buffer = renderer.getBuffer(WATER_RENDER_TYPE);
+			IVertexBuilder buffer = WATER_MATERIAL.getBuffer(renderer, RenderType::getEntityTranslucent);
 			// Render model
 			model.render(matrixStackIn, buffer, combinedLightIn, combinedOverlayIn, color.r, color.g, color.b, color.a);
 		}
