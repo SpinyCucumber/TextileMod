5a8b79645a84a4cfcdb1000ff55fa4a001777448
Elijah Hilty <spinycucumber@gmail.com>
2020-08-27 21:59:26 -0500

    Added some info to ColorHelper

--- src/main/java/spinyq/spinytextiles/utility/ColorHelper.java
+++ src/main/java/spinyq/spinytextiles/utility/ColorHelper.java
@@ -2,6 +2,13 @@ package spinyq.spinytextiles.utility;
 
 public class ColorHelper {
 
+	/**
+	 * 
+	 * @param base
+	 * @param added
+	 * @param amount A float from 0 to 1
+	 * @return
+	 */
 	public static Color3f mixRealistic(Color3f base, Color3f added, float amount) {
 		// TODO
 		return base.lerp(added, amount);
