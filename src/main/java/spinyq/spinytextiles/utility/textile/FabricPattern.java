cae82d74aad50a0ba7641db00ebc29e557a61d28
ethilty <ethilty@crimson.ua.edu>
2020-09-13 01:40:55 -0500

    Fleshed out FabricPattern more

--- src/main/java/spinyq/spinytextiles/utility/textile/FabricPattern.java
+++ src/main/java/spinyq/spinytextiles/utility/textile/FabricPattern.java
@@ -1,17 +1,49 @@
 package spinyq.spinytextiles.utility.textile;
 
-import com.google.common.collect.ImmutableList;
+import java.util.SortedMap;
 
+import net.minecraft.util.ResourceLocation;
+
+/**
+ * A pattern used to make fabric in the loom.
+ * Consists of multiple layers, each of which has a name and texture.
+ * Colors are applied to layers based on the thread used to make the fabric.
+ * @author Elijah Hilty
+ *
+ */
 public class FabricPattern extends AbstractPattern<FabricPattern> {
 
-	private ImmutableList<String> colors;
+	public static class Builder {
+		
+		private SortedMap<String, ResourceLocation> layers;
+		
+		public Builder withLayer(String layerName, ResourceLocation texture) {
+			layers.put(layerName, texture);
+			return this;
+		}
+		
+		public Builder withLayer(String layerName) {
+			// TODO Generate texture location using layer name and pattern registry name.
+			// Check out the Item class?
+			return this;
+		}
+		
+		public FabricPattern build() {
+			return new FabricPattern(layers);
+		}
+		
+	}
+	
+	// A mapping between a string parameter that represents a layer with a color, and a texture.
+	// Preserves order so textures are displayed correctly.
+	private SortedMap<String, ResourceLocation> layers;
 
-	public FabricPattern(ImmutableList<String> colors) {
-		this.colors = colors;
+	public FabricPattern(SortedMap<String, ResourceLocation> layers) {
+		this.layers = layers;
 	}
 
-	public ImmutableList<String> getColors() {
-		return colors;
-	} 
-	
+	public SortedMap<String, ResourceLocation> getLayers() {
+		return layers;
+	}
+
 }
