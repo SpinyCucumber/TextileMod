08798a941db68b15eaeae0ec7bf3429e5f792253
ethilty <ethilty@crimson.ua.edu>
2020-09-19 17:42:44 -0500

    Fleshed out ModPatterns a bit more. Broken atm

--- src/main/java/spinyq/spinytextiles/ModPatterns.java
+++ src/main/java/spinyq/spinytextiles/ModPatterns.java
@@ -1,24 +1,33 @@
 package spinyq.spinytextiles;
 
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryBuilder;
+import spinyq.spinytextiles.utility.textile.FabricPattern;
+import spinyq.spinytextiles.utility.textile.GarmentPattern;
 
 public class ModPatterns {
-
-	// TODO Create deferred registers
+	
+	@SubscribeEvent
+	public static void createRegistries(RegistryEvent.NewRegistry event) {
+		// TODO Create general pattern registry?
+		new RegistryBuilder<FabricPattern>().setType(FabricPattern.class).setName(new ResourceLocation(TextileMod.MODID, "fabric_pattern")).create();
+		new RegistryBuilder<GarmentPattern>().setType(GarmentPattern.class).setName(new ResourceLocation(TextileMod.MODID, "garment_pattern")).create();
+	}
+	
+	public static final DeferredRegister<FabricPattern> FABRIC_PATTERNS = new DeferredRegister<>(GameRegistry.findRegistry(FabricPattern.class), TextileMod.MODID);
+	public static final DeferredRegister<GarmentPattern> GARMENT_PATTERNS = new DeferredRegister<>(GameRegistry.findRegistry(GarmentPattern.class), TextileMod.MODID);
 	
 	public static void init() {
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 		bus.register(ModPatterns.class);
-	}
-	
-	@SubscribeEvent
-	public static void createRegistries(RegistryEvent.NewRegistry event) {
-		// TODO Create fabric pattern registry
-		// TODO Create garment pattern registry
-		// TODO Create general pattern registry
+		FABRIC_PATTERNS.register(bus);
+		GARMENT_PATTERNS.register(bus);
 	}
 	
 }
