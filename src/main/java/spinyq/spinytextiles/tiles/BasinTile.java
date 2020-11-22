f08e412a9dfabff39734fc39ec0b757fcb66c13e
ethilty <ethilty@crimson.ua.edu>
2020-11-21 22:05:54 -0600

    Finished read, write methods

--- src/main/java/spinyq/spinytextiles/tiles/BasinTile.java
+++ src/main/java/spinyq/spinytextiles/tiles/BasinTile.java
@@ -4,6 +4,8 @@ import java.util.Set;
 import java.util.Stack;
 import java.util.function.Supplier;
 
+import com.google.common.collect.BiMap;
+import com.google.common.collect.ImmutableBiMap;
 import com.google.common.collect.ImmutableSet;
 
 import net.minecraft.entity.player.PlayerEntity;
@@ -13,6 +15,8 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.INBT;
+import net.minecraft.nbt.ListNBT;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SUpdateTileEntityPacket;
 import net.minecraft.tileentity.TileEntity;
@@ -25,8 +29,10 @@ import spinyq.spinytextiles.ModTags;
 import spinyq.spinytextiles.ModTiles;
 import spinyq.spinytextiles.items.IBleachableItem;
 import spinyq.spinytextiles.items.IDyeableItem;
-import spinyq.spinytextiles.tiles.BasinTile.State.EmptyState;
+import spinyq.spinytextiles.tiles.BasinTile.State.FilledState;
 import spinyq.spinytextiles.utility.ContainedItemStack;
+import spinyq.spinytextiles.utility.NBTHelper;
+import spinyq.spinytextiles.utility.NBTHelper.INBTPolymorphic;
 import spinyq.spinytextiles.utility.color.RYBKColor;
 import spinyq.spinytextiles.utility.textile.IBleachProvider;
 import spinyq.spinytextiles.utility.textile.IDyeProvider;
@@ -41,8 +47,13 @@ public class BasinTile extends TileEntity {
 
 	private static final int MAX_WATER_LEVEL = 8;
 	private static final float LYE_VALUE = 0.25f;
+	
+	private static final String STATE_TAG = "State";
 
-	public static abstract class State {
+	private static final BiMap<String, Supplier<State>> STATE_MAP = ImmutableBiMap.of("Empty", EmptyState::new, "Filled", FilledState::new,
+			"Dye", DyeState::new, "Bleach", BleachState::new);
+	
+	public static abstract class State implements INBTPolymorphic<State> {
 
 		protected BasinTile basin;
 		protected State superState, subState;
@@ -55,33 +66,14 @@ public class BasinTile extends TileEntity {
 		public State getSuperState() {
 			return superState;
 		}
-
-		public static class EmptyState extends State {
-
-			@Override
-			public ActionResultType onInteract(PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
-				// Get itemstack and item
-				ItemStack itemstack = player.getHeldItem(handIn);
-				Item item = itemstack.getItem();
-				// If player is holding a water bucket, empty the bucket and fill the cauldron.
-				if (item == Items.WATER_BUCKET) {
-
-					if (!basin.world.isRemote) {
-						if (!player.abilities.isCreativeMode) {
-							player.setHeldItem(handIn, new ItemStack(Items.BUCKET));
-						}
-						basin.swapState(this, new FilledState());
-						basin.world.playSound((PlayerEntity) null, basin.pos, SoundEvents.ITEM_BUCKET_EMPTY,
-								SoundCategory.BLOCKS, 1.0F, 1.0F);
-					}
-
-					return ActionResultType.SUCCESS;
-
-				}
-				return ActionResultType.PASS;
-			}
-
+		
+		@Override
+		public CompoundNBT serializeNBT() {
+			return new CompoundNBT();
 		}
+
+		@Override
+		public void deserializeNBT(CompoundNBT nbt) { }
 
 		public static class FilledState extends State {
 
@@ -120,140 +112,223 @@ public class BasinTile extends TileEntity {
 				return ActionResultType.PASS;
 			}
 
-		}
-
-		public static abstract class SaturatedState extends State {
-
-			public abstract boolean consumeInteraction(PlayerEntity player, Hand handIn, BlockRayTraceResult hit);
-			public abstract ActionResultType finishInteraction(PlayerEntity player, Hand handIn, BlockRayTraceResult hit);
-			
 			@Override
-			public ActionResultType onInteract(PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
-				// If we can consume, return success
-				if (consumeInteraction(player, handIn, hit))
-					return ActionResultType.SUCCESS;
-				else
-					return finishInteraction(player, handIn, hit);
+			public Supplier<State> getFactory() {
+				return FilledState::new;
+			}
+
+			@Override
+			public CompoundNBT serializeNBT() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public void deserializeNBT(CompoundNBT nbt) {
+				// TODO Auto-generated method stub
+				
 			}
 
 		}
 
-		public static class DyeState extends SaturatedState implements IDyeProvider {
+	}
 
-			private RYBKColor color = new RYBKColor();
-
-			@Override
-			public RYBKColor getColor() {
-				return color;
-			}
-
-			@Override
-			public boolean drain(int amount) {
-				return ((FilledState) getSuperState()).drain(amount);
-			}
-
-			@Override
-			public boolean consumeInteraction(PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
-				// Get itemstack and item
-				ItemStack itemStack = player.getHeldItem(handIn);
-				Item item = itemStack.getItem();
-				if (!(item instanceof DyeItem))
-					return false;
-				// Retrieve the color of the dye
-				DyeItem dye = (DyeItem) item;
-				RYBKColor dyeColor = new RYBKColor().fromDye(dye.getDyeColor());
-				// If new color didn't change at all, don't accept dye
-				RYBKColor newColor = color.plus(dyeColor).clamp();
-				if (newColor.equals(color))
-					return false;
-
+	public static class EmptyState extends State {
+	
+		@Override
+		public ActionResultType onInteract(PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+			// Get itemstack and item
+			ItemStack itemstack = player.getHeldItem(handIn);
+			Item item = itemstack.getItem();
+			// If player is holding a water bucket, empty the bucket and fill the cauldron.
+			if (item == Items.WATER_BUCKET) {
+	
 				if (!basin.world.isRemote) {
-					// Consume one item if player is not in creative
 					if (!player.abilities.isCreativeMode) {
-						itemStack.shrink(1);
+						player.setHeldItem(handIn, new ItemStack(Items.BUCKET));
 					}
-					// Change the color
-					color = newColor;
+					basin.swapState(this, new FilledState());
 					basin.world.playSound((PlayerEntity) null, basin.pos, SoundEvents.ITEM_BUCKET_EMPTY,
 							SoundCategory.BLOCKS, 1.0F, 1.0F);
 				}
-
-				return true;
+	
+				return ActionResultType.SUCCESS;
+	
 			}
-
-			@Override
-			public ActionResultType finishInteraction(PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
-				// Get itemstack and item
-				ItemStack itemStack = player.getHeldItem(handIn);
-				Item item = itemStack.getItem();
-				// If item is dyeable, dye it
-				if (item instanceof IDyeableItem) {
-					IDyeableItem dyeable = (IDyeableItem) item;
-					ContainedItemStack<PlayerInventory> containedStack = new ContainedItemStack<>(itemStack, player.inventory);
-					if (dyeable.dye(containedStack, this)) return ActionResultType.SUCCESS;
-				}
-				// TODO Glowstone saturation modifier
-				return ActionResultType.PASS;
-			}
-
+			return ActionResultType.PASS;
 		}
 
-		public static class BleachState extends SaturatedState implements IBleachProvider {
+		@Override
+		public Supplier<State> getFactory() {
+			return EmptyState::new;
+		}
+	
+	}
 
-			private float bleachLevel = 0.0f;
+	public static abstract class SaturatedState extends State {
+	
+		public abstract boolean consumeInteraction(PlayerEntity player, Hand handIn, BlockRayTraceResult hit);
+		public abstract ActionResultType finishInteraction(PlayerEntity player, Hand handIn, BlockRayTraceResult hit);
+		
+		@Override
+		public ActionResultType onInteract(PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+			// If we can consume, return success
+			if (consumeInteraction(player, handIn, hit))
+				return ActionResultType.SUCCESS;
+			else
+				return finishInteraction(player, handIn, hit);
+		}
+	
+	}
+
+	public static class DyeState extends SaturatedState implements IDyeProvider {
+	
+		private RYBKColor color = new RYBKColor();
+	
+		@Override
+		public RYBKColor getColor() {
+			return color;
+		}
+	
+		@Override
+		public boolean drain(int amount) {
+			return ((FilledState) getSuperState()).drain(amount);
+		}
+	
+		@Override
+		public boolean consumeInteraction(PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+			// Get itemstack and item
+			ItemStack itemStack = player.getHeldItem(handIn);
+			Item item = itemStack.getItem();
+			if (!(item instanceof DyeItem))
+				return false;
+			// Retrieve the color of the dye
+			DyeItem dye = (DyeItem) item;
+			RYBKColor dyeColor = new RYBKColor().fromDye(dye.getDyeColor());
+			// If new color didn't change at all, don't accept dye
+			RYBKColor newColor = color.plus(dyeColor).clamp();
+			if (newColor.equals(color))
+				return false;
+	
+			if (!basin.world.isRemote) {
+				// Consume one item if player is not in creative
+				if (!player.abilities.isCreativeMode) {
+					itemStack.shrink(1);
+				}
+				// Change the color
+				color = newColor;
+				basin.world.playSound((PlayerEntity) null, basin.pos, SoundEvents.ITEM_BUCKET_EMPTY,
+						SoundCategory.BLOCKS, 1.0F, 1.0F);
+			}
+	
+			return true;
+		}
+	
+		@Override
+		public ActionResultType finishInteraction(PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+			// Get itemstack and item
+			ItemStack itemStack = player.getHeldItem(handIn);
+			Item item = itemStack.getItem();
+			// If item is dyeable, dye it
+			if (item instanceof IDyeableItem) {
+				IDyeableItem dyeable = (IDyeableItem) item;
+				ContainedItemStack<PlayerInventory> containedStack = new ContainedItemStack<>(itemStack, player.inventory);
+				if (dyeable.dye(containedStack, this)) return ActionResultType.SUCCESS;
+			}
+			// TODO Glowstone saturation modifier
+			return ActionResultType.PASS;
+		}
+
+		@Override
+		public Supplier<State> getFactory() {
+			return DyeState::new;
+		}
+
+		@Override
+		public CompoundNBT serializeNBT() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void deserializeNBT(CompoundNBT nbt) {
+			// TODO Auto-generated method stub
 			
-			@Override
-			public float getBleachLevel() {
-				return bleachLevel;
-			}
+		}
+	
+	}
 
-			@Override
-			public boolean drain(int amount) {
-				return ((FilledState) getSuperState()).drain(amount);
-			}
-
-			@Override
-			public boolean consumeInteraction(PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
-				// Get itemstack and item
-				ItemStack itemStack = player.getHeldItem(handIn);
-				Item item = itemStack.getItem();
-				if (!item.getTags().contains(ModTags.LYE_TAG))
-					return false;
-				// If bleach level didn't change at all, don't accept bleach
-				float newBleachLevel = Math.min(bleachLevel + LYE_VALUE, 1.0f);
-				if (newBleachLevel== bleachLevel)
-					return false;
-
-				if (!basin.world.isRemote) {
-					// Consume one item if player is not in creative
-					if (!player.abilities.isCreativeMode) {
-						itemStack.shrink(1);
-					}
-					// Change the bleach level
-					bleachLevel = newBleachLevel;
-					basin.world.playSound((PlayerEntity) null, basin.pos, SoundEvents.ITEM_BUCKET_EMPTY,
-							SoundCategory.BLOCKS, 1.0F, 1.0F);
+	public static class BleachState extends SaturatedState implements IBleachProvider {
+	
+		private float bleachLevel = 0.0f;
+		
+		@Override
+		public float getBleachLevel() {
+			return bleachLevel;
+		}
+	
+		@Override
+		public boolean drain(int amount) {
+			return ((FilledState) getSuperState()).drain(amount);
+		}
+	
+		@Override
+		public boolean consumeInteraction(PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+			// Get itemstack and item
+			ItemStack itemStack = player.getHeldItem(handIn);
+			Item item = itemStack.getItem();
+			if (!item.getTags().contains(ModTags.LYE_TAG))
+				return false;
+			// If bleach level didn't change at all, don't accept bleach
+			float newBleachLevel = Math.min(bleachLevel + LYE_VALUE, 1.0f);
+			if (newBleachLevel== bleachLevel)
+				return false;
+	
+			if (!basin.world.isRemote) {
+				// Consume one item if player is not in creative
+				if (!player.abilities.isCreativeMode) {
+					itemStack.shrink(1);
 				}
-
-				return true;
+				// Change the bleach level
+				bleachLevel = newBleachLevel;
+				basin.world.playSound((PlayerEntity) null, basin.pos, SoundEvents.ITEM_BUCKET_EMPTY,
+						SoundCategory.BLOCKS, 1.0F, 1.0F);
 			}
-
-			@Override
-			public ActionResultType finishInteraction(PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
-				// Get itemstack and item
-				ItemStack itemStack = player.getHeldItem(handIn);
-				Item item = itemStack.getItem();
-				// If item is bleachable, bleach it
-				if (item instanceof IDyeableItem) {
-					IBleachableItem bleachable = (IBleachableItem) item;
-					ContainedItemStack<PlayerInventory> containedStack = new ContainedItemStack<>(itemStack, player.inventory);
-					if (bleachable.bleach(containedStack, this)) return ActionResultType.SUCCESS;
-				}
-				return ActionResultType.PASS;
+	
+			return true;
+		}
+	
+		@Override
+		public ActionResultType finishInteraction(PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+			// Get itemstack and item
+			ItemStack itemStack = player.getHeldItem(handIn);
+			Item item = itemStack.getItem();
+			// If item is bleachable, bleach it
+			if (item instanceof IDyeableItem) {
+				IBleachableItem bleachable = (IBleachableItem) item;
+				ContainedItemStack<PlayerInventory> containedStack = new ContainedItemStack<>(itemStack, player.inventory);
+				if (bleachable.bleach(containedStack, this)) return ActionResultType.SUCCESS;
 			}
-
+			return ActionResultType.PASS;
 		}
 
+		@Override
+		public Supplier<State> getFactory() {
+			return BleachState::new;
+		}
+
+		@Override
+		public CompoundNBT serializeNBT() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void deserializeNBT(CompoundNBT nbt) {
+			// TODO Auto-generated method stub
+			
+		}
+	
 	}
 
 	private Stack<State> stack = new Stack<>();
@@ -291,14 +366,30 @@ public class BasinTile extends TileEntity {
 
 	@Override
 	public void read(CompoundNBT compound) {
-		// TODO Auto-generated method stub
 		super.read(compound);
+		// Retrieve a list NBT
+		ListNBT listNBT = compound.getList(STATE_TAG, 10);
+		// Convert each list element to a new state, and add them to our stack
+		for (INBT elementNBT : listNBT) {
+			CompoundNBT objectNBT = (CompoundNBT) elementNBT;
+			State state = NBTHelper.readPolymorphic(objectNBT, STATE_MAP::get);
+			pushState(state);
+		}
 	}
 
 	@Override
 	public CompoundNBT write(CompoundNBT compound) {
-		// TODO Auto-generated method stub
-		return super.write(compound);
+		CompoundNBT result = super.write(compound);
+		// Create a new ListNBT
+		ListNBT listNBT = new ListNBT();
+		// Write each state to the list
+		for (State state : stack) {
+			CompoundNBT objectNBT = NBTHelper.writePolymorphic(state, STATE_MAP.inverse()::get);
+			listNBT.add(objectNBT);
+		}
+		// Set value and return
+		result.put(STATE_TAG, listNBT);
+		return result;
 	}
 
 	@Override
