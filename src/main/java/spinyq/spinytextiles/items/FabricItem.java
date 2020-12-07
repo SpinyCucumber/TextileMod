5f7cadc11705949ab469ef56e7d50daa95f992b8
ethilty <ethilty@crimson.ua.edu>
2020-12-06 22:39:48 -0600

    Added some methods in FabricItem

--- src/main/java/spinyq/spinytextiles/items/FabricItem.java
+++ src/main/java/spinyq/spinytextiles/items/FabricItem.java
@@ -2,12 +2,25 @@ package spinyq.spinytextiles.items;
 
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import spinyq.spinytextiles.utility.ContainedItemStack;
+import spinyq.spinytextiles.utility.NBTHelper;
+import spinyq.spinytextiles.utility.textile.FabricInfo;
 import spinyq.spinytextiles.utility.textile.IBleachProvider;
 import spinyq.spinytextiles.utility.textile.IDyeProvider;
 
 public class FabricItem extends Item implements IDyeableItem, IBleachableItem {
 
+	private static final String INFO_TAG = "Info";
+	
+	public FabricInfo getInfo(ItemStack stack) {
+		return NBTHelper.getOrNull(FabricInfo::new, stack.getOrCreateTag(), INFO_TAG);
+	}
+	
+	public void setInfo(ItemStack stack, FabricInfo info) {
+		NBTHelper.put(stack.getOrCreateTag(), INFO_TAG, info);
+	}
+	
 	public FabricItem(Properties properties) {
 		super(properties);
 		// TODO Set up color handler
