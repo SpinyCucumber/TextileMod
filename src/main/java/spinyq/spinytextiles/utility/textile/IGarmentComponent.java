fcc0f30d58de26984ca989df20ea0aba096dccd0
ethilty <ethilty@crimson.ua.edu>
2020-12-06 22:29:45 -0600

    Removed old stuff in IGarmentComponent

--- src/main/java/spinyq/spinytextiles/utility/textile/IGarmentComponent.java
+++ src/main/java/spinyq/spinytextiles/utility/textile/IGarmentComponent.java
@@ -1,18 +1,8 @@
 package spinyq.spinytextiles.utility.textile;
 
-import java.util.Map;
-import java.util.function.Supplier;
-
-import com.google.common.collect.ImmutableMap;
-
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraftforge.common.util.INBTSerializable;
 
 public interface IGarmentComponent extends INBTSerializable<CompoundNBT> {
-
-	static final Map<String, Supplier<IGarmentComponent>> factoryMap = new ImmutableMap.Builder<String, Supplier<IGarmentComponent>>()
-			.put("fiber", FiberInfo::new)
-			.put("fabric", FabricInfo::new)
-			.build();
 		
 }
