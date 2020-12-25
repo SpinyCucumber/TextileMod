34aee4980e385e2413ccca3c27028458821b460f
ethilty <spinycucumber@gmail.com>
2020-12-25 17:27:46 -0600

    i18n methods in FabricPattern

--- src/main/java/spinyq/spinytextiles/utility/textile/FabricPattern.java
+++ src/main/java/spinyq/spinytextiles/utility/textile/FabricPattern.java
@@ -2,6 +2,8 @@ package spinyq.spinytextiles.utility.textile;
 
 import com.google.common.collect.ImmutableList;
 
+import net.minecraft.util.Util;
+
 /**
  * A pattern used to make fabric in the loom.
  * Consists of multiple layers, represented by a name.
@@ -14,6 +16,7 @@ public class FabricPattern extends AbstractPattern<FabricPattern> {
 	
 	// An ordered list of the fabric layers
 	private final ImmutableList<String> layers;
+	private String translationKey;
 
 	public ImmutableList<String> getLayers() {
 		return layers;
@@ -25,6 +28,23 @@ public class FabricPattern extends AbstractPattern<FabricPattern> {
 	
 	public FabricPattern(String...layers) {
 		this(ImmutableList.copyOf(layers));
+	}
+	
+	/**
+	 * Returns a translation key which can can be translated into the name of this fabric pattern.
+	 * For example, this might translate as "Vertical Stripes" or "Checkerboard"
+	 */
+	public String getTranslationKey() {
+		if (translationKey == null) translationKey = Util.makeTranslationKey("fabricpattern", getRegistryName());
+		return translationKey;
+	}
+	
+	/**
+	 * Returns a translation key which can be translated to a description of this fabric pattern.
+	 * For example, this might translate as "striped" in English for a striped fabric pattern.
+	 */
+	public String getDescriptionTranslationKey() {
+		return getTranslationKey() + ".description";
 	}
 
 }
