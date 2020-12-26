cbc4fd8ece0c46f005084fd4d55e036a93503614
ethilty <spinycucumber@gmail.com>
2020-12-26 16:44:24 -0600

    Added equals and hashCode to RGBColor

--- src/main/java/spinyq/spinytextiles/utility/color/RGBColor.java
+++ src/main/java/spinyq/spinytextiles/utility/color/RGBColor.java
@@ -1,5 +1,6 @@
 package spinyq.spinytextiles.utility.color;
 
+import java.util.Objects;
 import java.util.Random;
 
 /**
@@ -126,6 +127,27 @@ public class RGBColor {
 	@Override
 	public String toString() {
 		return "Color [r=" + r + ", g=" + g + ", b=" + b + "]";
+	}
+	
+	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(b, g, r);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RGBColor other = (RGBColor) obj;
+		return Float.floatToIntBits(b) == Float.floatToIntBits(other.b)
+				&& Float.floatToIntBits(g) == Float.floatToIntBits(other.g)
+				&& Float.floatToIntBits(r) == Float.floatToIntBits(other.r);
 	}
 
 	/**
