5c67dd1f1861994ab9e550d5a8345a6a2c537197
ethilty <spinycucumber@gmail.com>
2020-12-26 13:13:25 -0600

    Removed FabricTextureManager

--- src/main/java/spinyq/spinytextiles/TextileMod.java
+++ src/main/java/spinyq/spinytextiles/TextileMod.java
@@ -1,17 +1,11 @@
 package spinyq.spinytextiles;
 
 import net.minecraftforge.fml.common.Mod;
-import spinyq.spinytextiles.client.model.FabricTextureManager;
 
 @Mod(TextileMod.MODID)
 public class TextileMod
 {
 	
 	public static final String MODID = "spinytextiles";
-    
-    public TextileMod() {
-    	// Send message to other classes when the mod has been constructed
-    	FabricTextureManager.INSTANCE.onModConstructed();
-    }
     
 }
