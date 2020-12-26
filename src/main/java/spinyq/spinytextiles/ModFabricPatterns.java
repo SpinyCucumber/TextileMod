5d53bd07d6f6ae72cdc64e9b891e3b16c837ec50
ethilty <spinycucumber@gmail.com>
2020-12-26 13:04:11 -0600

    ModFabricPatterns uses FabricLayer

--- src/main/java/spinyq/spinytextiles/ModFabricPatterns.java
+++ src/main/java/spinyq/spinytextiles/ModFabricPatterns.java
@@ -18,18 +18,22 @@ public class ModFabricPatterns {
 	public static final DeferredRegister<FabricPattern> FABRIC_PATTERNS = new DeferredRegister<>(
 			LazyForgeRegistry.of(FabricPattern.class), TextileMod.MODID);
 
-	public static final RegistryObject<FabricPattern> SOLID = FABRIC_PATTERNS.register("solid", () -> new FabricPattern("base")),
-			HORIZONTAL_STRIPES = FABRIC_PATTERNS.register("horizontal_stripes", () -> new FabricPattern("base", "stripes")),
-			VERTICAL_STRIPES = FABRIC_PATTERNS.register("vertical_stripes", () -> new FabricPattern("base", "stripes")),
-			DIAGONAL_STRIPES = FABRIC_PATTERNS.register("diagonal_stripes", () -> new FabricPattern("base", "stripes")),
-			DOTS = FABRIC_PATTERNS.register("dots", () -> new FabricPattern("base", "dots"));
+	public static final RegistryObject<FabricPattern> SOLID = FABRIC_PATTERNS.register("solid",
+			() -> new FabricPattern(ModFabricLayers.BASE)),
+			HORIZONTAL_STRIPES = FABRIC_PATTERNS.register("horizontal_stripes",
+					() -> new FabricPattern(ModFabricLayers.BASE, ModFabricLayers.HORIZONTAL_STRIPES)),
+			VERTICAL_STRIPES = FABRIC_PATTERNS.register("vertical_stripes",
+					() -> new FabricPattern(ModFabricLayers.BASE, ModFabricLayers.VERTICAL_STRIPES)),
+			DIAGONAL_STRIPES = FABRIC_PATTERNS.register("diagonal_stripes",
+					() -> new FabricPattern(ModFabricLayers.BASE, ModFabricLayers.DIAGONAL_STRIPES)),
+			DOTS = FABRIC_PATTERNS.register("dots",
+					() -> new FabricPattern(ModFabricLayers.BASE, ModFabricLayers.DOTS));
 
 	@SubscribeEvent
 	public static void createRegistries(RegistryEvent.NewRegistry event) {
 		// Create registry
 		new RegistryBuilder<FabricPattern>().setType(FabricPattern.class)
-				.setName(new ResourceLocation(TextileMod.MODID, "fabricpattern"))
-				.create();
+				.setName(new ResourceLocation(TextileMod.MODID, "fabricpattern")).create();
 		// Hook up deferred register
 		FABRIC_PATTERNS.register(FMLJavaModLoadingContext.get().getModEventBus());
 	}
