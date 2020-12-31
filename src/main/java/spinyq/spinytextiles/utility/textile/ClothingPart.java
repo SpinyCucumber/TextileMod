2203febe21842c2087e0aa233e99fe7c1fbd1717
ethilty <spinycucumber@gmail.com>
2020-12-31 11:41:42 -0600

    Created ClothingPart

---
+++ src/main/java/spinyq/spinytextiles/utility/textile/ClothingPart.java
@@ -0,0 +1,7 @@
+package spinyq.spinytextiles.utility.textile;
+
+import net.minecraftforge.registries.ForgeRegistryEntry;
+
+public class ClothingPart extends ForgeRegistryEntry<ClothingPart> {
+
+}
--- src/main/java/spinyq/spinytextiles/utility/textile/ClothingPattern.java
+++ src/main/java/spinyq/spinytextiles/utility/textile/ClothingPattern.java
@@ -1,7 +1,25 @@
 package spinyq.spinytextiles.utility.textile;
 
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import com.google.common.collect.ImmutableSet;
+
 public class ClothingPattern extends AbstractPattern<ClothingPattern> {
 
-	// TODO
+	private final ImmutableSet<Supplier<ClothingPart>> parts;
+
+	private ClothingPattern(ImmutableSet<Supplier<ClothingPart>> parts) {
+		this.parts = parts;
+	}
+	
+	@SafeVarargs
+	public ClothingPattern(Supplier<ClothingPart>...parts) {
+		this(ImmutableSet.copyOf(parts));
+	}
+	
+	public Stream<ClothingPart> getPartStream() {
+		return parts.stream().map(Supplier::get);
+	}
 	
 }
