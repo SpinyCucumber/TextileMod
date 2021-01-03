e38380cdd9ebdbd849b21c5e3b396b76462b449b
ethilty <ethilty@crimson.ua.edu>
2021-01-03 15:01:15 -0600

    FabricClothingPartRenderer 1st draft

--- src/main/java/spinyq/spinytextiles/client/render/FabricClothingPartRenderer.java
+++ src/main/java/spinyq/spinytextiles/client/render/FabricClothingPartRenderer.java
@@ -4,9 +4,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
+import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Random;
 import java.util.stream.Stream;
 
 import net.minecraft.client.renderer.model.BakedQuad;
@@ -20,6 +22,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.client.model.SimpleModelTransform;
+import net.minecraftforge.client.model.data.EmptyModelData;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import spinyq.spinytextiles.client.render.ClothingRenderer.BodyPart;
@@ -27,11 +30,13 @@ import spinyq.spinytextiles.client.render.ClothingRenderer.IClothingPartRenderer
 import spinyq.spinytextiles.utility.textile.clothing.FabricClothingPart;
 import spinyq.spinytextiles.utility.textile.clothing.IClothing;
 
+// TODO Rewrite without using BlockModel
 @OnlyIn(Dist.CLIENT)
 public class FabricClothingPartRenderer implements IClothingPartRenderer<FabricClothingPart> {
 
-	private Map<FabricClothingPart, IBakedModel> bakedModels = new HashMap<>();
-	private Map<FabricClothingPart, BlockModel> unbakedModels = new HashMap<>();
+	private Map<FabricClothingPart, EnumMap<BodyPart, IBakedModel>> bakedModels = new HashMap<>();
+	private Map<FabricClothingPart, EnumMap<BodyPart, BlockModel>> unbakedModels = new HashMap<>();
+	private final Random random = new Random();
 	
 	public FabricClothingPartRenderer() {
 		// Hook up event handlers
@@ -45,34 +50,57 @@ public class FabricClothingPartRenderer implements IClothingPartRenderer<FabricC
 
 	@Override
 	public Stream<BakedQuad> getQuads(FabricClothingPart clothingPart, IClothing clothing, BodyPart bodyPart) {
-		// TODO Auto-generated method stub
-		return null;
+		EnumMap<BodyPart, IBakedModel> bodyPartMap = bakedModels.get(clothingPart);
+		if (bodyPartMap != null) {
+			IBakedModel model = bodyPartMap.get(bodyPart);
+			if (model != null) {
+				return model.getQuads(null, null, random, EmptyModelData.INSTANCE).stream();
+			}
+		}
+		return Stream.empty();
 	}
 
 	@Override
-	public void loadResources(IResourceManager resourceManager, FabricClothingPart part) throws IOException {
+	public void loadResources(IResourceManager resourceManager, FabricClothingPart clothingPart) throws IOException {
 		// Load the model of the fabric clothing part
-		ResourceLocation modelLocation = getModelLocation(part);
-		IResource resource = resourceManager.getResource(modelLocation);
-		Reader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8);
-		// Load model
-		unbakedModels.put(part, BlockModel.deserialize(reader));
+		EnumMap<BodyPart, BlockModel> bodyPartMap = new EnumMap<>(BodyPart.class);
+		for (BodyPart bodyPart : BodyPart.values()) {
+			ResourceLocation modelLocation = getModelLocation(clothingPart, bodyPart);
+			// Only load the model if it exists
+			if (resourceManager.hasResource(modelLocation)) {
+				IResource resource = resourceManager.getResource(modelLocation);
+				Reader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8);
+				// Load model
+				bodyPartMap.put(bodyPart, BlockModel.deserialize(reader));
+			}
+		}
+		// Put body part map into main map
+		unbakedModels.put(clothingPart, bodyPartMap);
 	}
 	
 	@SuppressWarnings("resource")
 	private void bakeModels(ModelBakery bakery) {
 		// For each unbaked model, bake it
-		for (Entry<FabricClothingPart, BlockModel> entry : unbakedModels.entrySet()) {
-			BlockModel model = entry.getValue();
-			ResourceLocation modelLocation = getModelLocation(entry.getKey());
-			IBakedModel bakedModel = model.bakeModel(bakery, model, bakery.getSpriteMap()::getSprite, SimpleModelTransform.IDENTITY, modelLocation, true);
-			bakedModels.put(entry.getKey(), bakedModel);
+		for (Entry<FabricClothingPart, EnumMap<BodyPart, BlockModel>> entry : unbakedModels.entrySet()) {
+
+			EnumMap<BodyPart, IBakedModel> bodyPartMap = new EnumMap<>(BodyPart.class);
+			for (Entry<BodyPart, BlockModel> subEntry : entry.getValue().entrySet()) {
+				ResourceLocation modelLocation = getModelLocation(entry.getKey(), subEntry.getKey());
+				BlockModel model = subEntry.getValue();
+				IBakedModel bakedModel = model.bakeModel(bakery, model, bakery.getSpriteMap()::getSprite, SimpleModelTransform.IDENTITY, modelLocation, true);
+				bodyPartMap.put(subEntry.getKey(), bakedModel);
+			}
+			
+			bakedModels.put(entry.getKey(), bodyPartMap);
 		}
+		// Clear unbaked models to save memory?
+		unbakedModels.clear();
 	}
 	
-	private ResourceLocation getModelLocation(FabricClothingPart part) {
+	private ResourceLocation getModelLocation(FabricClothingPart part, BodyPart bodyPart) {
 		ResourceLocation id = part.getRegistryName();
-		return new ResourceLocation(id.getNamespace(), "models/clothingpart/" + id.getPath() + ".json");
+		String bodyPartPath = bodyPart.toString().toLowerCase();
+		return new ResourceLocation(id.getNamespace(), "models/clothingpart/" + id.getPath() + "_" +  bodyPartPath + ".json");
 	}
 
 }
